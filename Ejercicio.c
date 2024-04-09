79e1861e4e3eae3cf3a33203e588c9f84a6f8d69
PastoraB <serpensfideliocanis@gmail.com>
2024-04-09 11:12:25 -0600

    Ejercicio1

---
+++ Ejercicio.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+int main()
+{
+     int num1, num2; 
+         printf("Como calcular el cubo de un numero: "); 
+        scanf("%i", &num1);
+
+        num2 = num1 * num1 * num1; 
+
+        printf("El numnero al cuadrado seria %i", num2); 
+
+    return 0;
+}
+
+
+
+
+ 
+    
+
