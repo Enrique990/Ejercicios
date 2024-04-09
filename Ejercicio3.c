e72f708bf1f29162473667e548a2538e7b069cb8
danilo-uam <164056215+danilo-uam@users.noreply.github.com>
2024-04-09 10:57:37 -0600

    Add files via upload

---
+++ Ejercicio3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char nombre[50];
+
+    printf ("Por favor, ingrese su nombre: ");
+    scanf("%s",nombre);
+
+printf("Hola %s\n",nombre);
+    return 0;
+}
